1efc05d57fe1948d12f9921f1ad9a3f1bf7ba842
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2020-04-30 00:10:18 -0300

    Alterando exemplo para utilizar Makefile invés de CMake

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -1,20 +1,2 @@
-#!/bin/sh
-
-ls -l /
-ls -l /opt/
-ls -l /opt/intel/
-ls -l /opt/intel/inteloneapi/
-
-source /opt/intel/inteloneapi/setvars.sh
-
-sample=$1
-
-mkdir build
-
-cd build
-
-cmake ../ -DSAMPLE_NAME=$sample 
-
-make 
-
-./$sample
+make
+make run
